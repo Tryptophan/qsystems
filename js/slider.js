a85724c375d466a8bafec15e89b99437ce3d9f14
Jacob Greenway <jagreenway12@gmail.com>
2015-05-10 18:03:38 -0500

    fixed slider and added all pages

--- js/slider.js
+++ js/slider.js
@@ -1,108 +1,14 @@
-// Slider Functions
-var count = $("#slider-images img").size();
+$(document).ready(function() {
 
-var sliderIndex = 1;
-var sliderNext = 2;
+	var slider = $(".bxslider").bxSlider();
 
-$(document).ready(function(){
-	startSlider();
-});
+	$("#next").click(function(){
+		slider.goToNextSlide();
+		return false;
+	});
 
-function startSlider() {
-	count = $("#slider-images img").size();
-
-	loop = setInterval(function() {
-
-		if(sliderNext > count){
-			sliderNext = 1;
-			sliderIndex = 1;
-		}
-
-		$("#slider-images img").fadeOut(600);
-		$("#slide-" + sliderNext).delay(600).fadeIn(600);
-
-		sliderIndex = sliderNext;
-		sliderNext = sliderNext + 1;
-
-	}, 6000);
-}
-
-// Next / Prev Functions
-
-$("#previous").hover(
-	function() {
-		if(!$(this).is(':animated')) {
-			$(this).animate({ left: 20 });
-			$(this).css('cursor','pointer');
-		}
-	}
-	, function() {
-		$(this).animate({ left: 30 });
-		$(this).css('cursor','pointer');
-	}
-);
-
-$("#next").hover(
-	function() {
-		if(!$(this).is(':animated')) {
-			$(this).animate({ right: 20 });
-			$(this).css("cursor", "pointer");
-		}
-	}
-	, function() {
-		$(this).animate({ right: 30 });
-		$(this).css("cursor", "pointer");
-	}
-);
-
-function prev() {
-	newSlide = sliderIndex - 1;
-	showSlide(newSlide, "left");
-}
-
-function next() {
-	newSlide = sliderIndex + 1;
-	showSlide(newSlide);
-}
-
-function stopLoop() {
-	window.clearInterval(loop);
-}
-
-function showSlide(id) {
-	stopLoop();
-	if (id > count) {
-		id = 1;
-	}
-
-	else if (id < 1) {
-		id = count;
-	}
-
-	$("#slider-images img").fadeOut(600);
-	$("#slide-" + id).delay(600).fadeIn(600);
-
-	sliderIndex = id;
-	sliderNext = id + 1;
-	startSlider();
-}
-
-$("#previous").click(function() {
-	prev();
-});
-
-$("#next").click(function() {
-	next();
-});
-
-$(".slider-button").hover(
-	function() {
-		if(!$(this).is(':animated')) {
-			$(this).animate({ bottom: 35 })
-			$(this).css("cursor", "pointer");
-		}
-	},
-	function() {
-		$(this).animate({ bottom: 40 })
-		$(this).css("cursor", "pointer");
+	$("#previous").click(function(){
+		slider.goToPrevSlide();
+		return false;
+	});
 });
